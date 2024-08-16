dba850e49c7d76c5c9011176da4a0f4611ef9e04
edwarfigueroa.GLOB <edwarfigueroa.glob@commandalkon.com>
2024-08-15 20:28:24 -0500

    Client-server node

--- client-results-screen/index.js
+++ client-results-screen/index.js
@@ -3,13 +3,14 @@ document.getElementById("fetch-button").addEventListener("click", fetchData);
 async function fetchData() {
   renderLoadingState();
   try {
-    const response = await fetch("http://localhost:3004/posts");
+    const response = await fetch("http://localhost:5050/users");
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
     const data = await response.json();
     renderData(data);
   } catch (error) {
+    console.error(error);
     renderErrorState();
   }
 }
@@ -32,11 +33,12 @@ function renderData(data) {
   const container = document.getElementById("data-container");
   container.innerHTML = ""; // Clear previous data
 
-  if (data.length > 0) {
-    data.forEach((item) => {
+  if (data.players.length > 0) {
+    data.players.forEach((item) => {
       const div = document.createElement("div");
       div.className = "item";
-      div.innerHTML = item.id;
+      div.innerHTML = item.name;
+      div.innerHTML = `<img src="${item.profilePicture}" /><p>${item.name}</p>`;
       container.appendChild(div);
     });
   }
